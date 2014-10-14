e32709cb80e6ebee08214cde2b45c01a8fa8eac2
Mingxi Li <lee8568@gmail.com>
2014-10-13 23:06:18 -0500

    merge sorted list recursively

---
+++ mergelistsrecursively.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	int val;
+	Node* next;
+}
+
+Node* merge(Node* p, Node* q){
+	if(!p) return q;
+	if(!q) return p;
+	Node* res=NULL;
+	if(p->val<q->val){
+		res=p;
+		res->next=merge(p->next,q);
+	}else{
+		res=q;
+		res->next=merge(p,q->next);
+	}
+}
